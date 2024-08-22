840ae6f498249b207cdfc01821a6413d70546e1d
piresz0 <renan090604@edu.unipar.br>
2024-08-22 07:54:16 -0300

    Criação do projeto JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Luis";
+
+            /*
+            Operadores Lógicos
+            * > Maior
+            * >= Maior igual
+            * < Menor
+            * <= Menor igual
+            * == é igual
+            * != é diferente
+            * equal compara Textos
+            */
+
+        if (idadePessoa > 21) {
+            System.out.println("É maior que 21");
+
+        }else {
+            System.out.println("Não é maior que 21");
+        }
+
+
+
     }
 }
