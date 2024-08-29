9e0e976dc9ecf596bad39589c9c4a328703de3a5
piresz0 <renan090604@edu.unipar.br>
2024-08-29 06:54:46 -0300

    Criação do projeto JAVA

---
+++ src/ExemploCondicao2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ExemploCondicao2 {
+
+
+    //Quer adicionar um desconto gradativo
+    // Valor Maior que 1000 - 10%
+    // Valor Maior que 2000 - 20%
+    // Valor Maior que 3000 - 30%
+    // Valor Menor igual a 1000 - 0%
+
+    public static void main(String[] args) {
+
+        double valorBruto = 3000;
+        double desconto = 0d;
+
+        // Operadores Logicos
+        // && e
+        // || ou
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o valor Bruto : ");
+        valorBruto = scanner.nextDouble();
+
+        if (valorBruto > 1000 && valorBruto<= 2000) {
+             desconto = valorBruto * 0.10;
+        }
+        else if (valorBruto > 2000 && valorBruto < 3000) {
+             desconto = valorBruto * (20 / 100);
+        }
+        else if (valorBruto >= 3000) {
+             desconto = valorBruto * (30. / 100);
+        }
+
+        System.out.println("Valor do Bruto: " + valorBruto);
+        System.out.println("Valor do desconto é " + desconto);
+        System.out.println("Valor Liquido é :" + (valorBruto - desconto) );
+
+
+
+    }
+
+
+
+}
---
+++ src/ExemploScanner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExemploScanner {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome = "";
+        Integer idade = 0;
+        Double peso = 0.0;
+
+        System.out.println("Escreva seu nome : ");
+        nome = sc.next();
+
+        System.out.println("Escreva a idade : ");
+        idade = sc.nextInt();
+
+        System.out.println("Escreva a peso : ");
+        peso = sc.nextDouble();
+
+        System.out.println("Seu nome é : " + nome);
+        System.out.println("Sua idade é : " + idade);
+        System.out.println("Seu peso é : "  + peso);
+    }
+}
